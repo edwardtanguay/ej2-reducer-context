80ffbf822301512b40593af55173bcad17e7c924
Edward Tanguay (windows) <edward@tanguay.info>
2022-07-22 11:51:24 +0200

    useContext works

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,30 @@
 import './App.css'
+import { useContext } from 'react';
+import { AppContext } from './AppContext';
 
 function App() {
+  const { count, setCount } = useContext(AppContext);
 
-  return (
-    <div className="App">
-      test
-      
-    </div>
-  )
+    return (
+        <div className="App">
+            <h1>useContext / useReducer Site</h1>
+            <div>count = {count}</div>
+            <div className="buttonArea">
+                <button
+                    className="decrease"
+                    onClick={() => setCount(count - 1)}
+                >
+                    -
+                </button>
+                <button
+                    className="increase"
+                    onClick={() => setCount(count + 1)}
+                >
+                    +
+                </button>
+            </div>
+        </div>
+    );
 }
 
 export default App
